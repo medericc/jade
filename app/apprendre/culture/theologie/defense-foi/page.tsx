440146b5e01065c1e807aee4cb1d58efea561300
medericc <bleublanctour1@>
2026-05-17 07:33:50 +0200

    ee

---
+++ app/apprendre/culture/theologie/defense-foi/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import data from '@/data/culture/theologie.json'
+
+export default function Page() {
+  return (
+    <main className="p-6 space-y-10">
+
+      {data.map((block, index) => (
+        <section
+          key={index}
+          className="grid grid-cols-2 gap-6"
+        >
+          <div>
+            <p className="whitespace-pre-line">
+              {block.fr}
+            </p>
+          </div>
+
+         
+        </section>
+      ))}
+
+    </main>
+  )
+}
