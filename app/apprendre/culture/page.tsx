83a8ed22cbee7a3120530c3987e9d9db09a4cbda
medericc <bleublanctour1@>
2026-05-18 16:48:06 +0200

    ee

--- app/apprendre/culture/page.tsx
+++ app/apprendre/culture/page.tsx
@@ -8,16 +8,22 @@ export default function Page() {
       
       <h1 className="text-3xl font-bold">Culture</h1>
 
+{/* Condensé ultra-complet retraçant tous les événements, l'évolution sociale et l'identité de ce Béarn qui a conquis la France, une synthèse historique exhaustive. */}
+
+
       <div className="flex gap-4">
         <a href="culture/bearn" className="px-6 py-3 bg-black text-white rounded-xl">
           Béarn
         </a>
 
+
+
+{/* Synthèse des événements majeurs hors Béarn, retraçant l'évolution sociale et culturelle de la France jusqu'à nos jours */}
+
         <a href="culture/monde" className="px-6 py-3 bg-black text-white rounded-xl">
           Monde
         </a>
- {/* L’évangéliste Jean (qui reconnaissait l’autorité du pape Clément sur lui) retrace la Genèse et St Jean Damascène dira que Marie est la première née de la nouvelle création (“Anne portait le paradis en son sein”). Elle n’a pas besoin de l’eau du baptême car elle est l’arche (comme Noé, sauvé du péché, sur les eaux qui l’engloutisse, par la grâce de Dieu) et avec l’arche d’alliance on remporte toutes nos batailles ! */}
-
+{/* Défense concise et complète de la vraie foi chrétienne */}
         <a href="culture/theologie" className="px-6 py-3 bg-black text-white rounded-xl">
           Théologie
         </a>
