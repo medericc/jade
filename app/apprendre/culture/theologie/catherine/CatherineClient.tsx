e8a83f856792b8cd53cf4621f316211a5b494d39
medericc <bleublanctour1@>
2026-06-24 13:12:22 +0200

    ee

--- app/apprendre/culture/theologie/catherine/CatherineClient.tsx
+++ app/apprendre/culture/theologie/catherine/CatherineClient.tsx
@@ -36,31 +36,40 @@ const teachingsF = [
 ]
 
 const teachingsB = [
-  `Ne pas corriger un pécheur par peur ou polir ses phrases par confort social, c'est comme un médecin qui met un pansement sur une plaie purulente sans la nettoyer. On peut même accomplir soi-même sa pénitence car ce péché est seulement endormi en nous.`,
+  `Ne pas courridya û pecadou per pòu ou dousseya las soûes frases per counfòrt souciau, qu'éy coum û medecî qui hique û pansamén sus ûe plague pudentisse chéns aproupia-la. Que poudém médich feni éth-medich la soûe penitence permou qu'aquéste pecat qu'éy soulamen adroumit déns nousàudis.`,
 
-  `L'amour-propre est le poison du monde et produit tous les péchés. Il faut vouloir le salut des âmes et faire la volonté de Dieu, pas la sienne. Tout le bien et le mal se font par l'intermédiaire du prochain.`,
+  `L'amou-propi qu'éy lou pousoû dou moùndẹ e que produsech touts lous pecats. Que cau boùle lou saubet de las àmnes e ha la boulentat de Diu, noû pas la soûe. Tout lou bê e lou mau que se hèn per lou proche.`,
 
-  `Les pécheurs en tentant les hommes deviennent les instruments qui éprouvent et renforcent la vertu des serviteurs de Dieu. La vertu dans la difficulté a + de valeur que dans la joie, comme un jardinier qui perd son jardin et continue de travailler la terre (il aime vraiment). Un laïc qui obéit avec un immense amour a plus de mérite qu'un religieux qui obéit par simple habitude.`,
+  `Lous pecadous en tentan lous òmis que debiénen lous utìs qui hìquen à l'esprabe e qu'agalharden la bertut dous gouyes de Diu. La bertut déns lou mauaysit qu'a méy de balou que déns lou gay, coum û yardinè qui pèr lou soû casau e countinue de tribalha la tèrre (qu'ayme de bertat). Û laïque qui aubedi dap û gran amou qu'a méy de merite qu'û reliyious qui aubedi per abitude.`,
 
-  `On ne peut pas suivre le Christ à notre convenance il faut suivre son chemin d'humilité. Les pécheurs souffrent comme les saints mais leur souffrance est inutile. Un petit acte fait avec charité a + de poids que des souffrances subies sans amour. On souffre non pas pour soi, mais par amour pour Dieu et par douleur de voir les autres s'égarer.`,
+  `Ne se pot pas segui lou Crist au noùste coumbenénce, que cau segui lou soû camì d'umilitat. Lous pecadous que soufèren coum lous sènts més la loue soufrence n'éy pas utìlẹ. Û petit àctẹ hèyt dap caritat qu'a méy de pés qui douléncis subi chéns amou. Ne soufrim pas tad éth-medich, més per amou ta Diu e per la doulou de bédẹ lous àutẹs pèrdẹ-s.`,
 
-  `Ne jamais juger la volonté des autres et ne se sentir offensé que par celui qui offense Dieu. Si Dieu te fait sentir la souffrance de cette âme, c'est pour que tu l'aides. Même une pensée égoïste nuit au prochain, tu le prives d'une prière.`,
+ 
+ 
+ 
+  `Ne pas jaméy yudyat la boulentat dous àutẹs e ne-s senti aufensat soùnquẹ p'ou qui aufénse Diu. Si Diu que-t hè senti la doulou d'aquéste àmne, qu'éy ta que-t l'àydes. Medich ûe pensade egoïste que hè dou mau au proche, qu'ou payres d'ûe pregari.`,
 
-  `Dieu veut que le riche ait besoin du pauvre et que le savant ait besoin de l'ignorant. Pour nous forcer à la charité. Si tu avais tous les talents, tu n'aurais pas besoin des autres et tu tomberais dans l'orgueil. Dieu nous utilise comme ses mains (ministres) pour distribuer ses dons. Si tu as quelque chose, c'est pour le donner à celui qui ne l'a pas.`,
+  `Diu bòu que lou rìche àyẹ besougn dou pràubẹ e qu'ou saben àyi besougn de l'ignouren. Ta-ns hourça à la caritat. Si abès touts lous talénts, ne-t caleré pas lou moùndẹ e que caderés héns l'ourgulh. Diu que-ns emplegue coum las soûes mâs ta desparti lous soûs dats. Si qu'as quàuqu'arré, qu'éy ta-u da au qui n'a pas.`,
 
-  `Si tu n'utilises pas ta raison pour chercher la Vérité, tu marches à l'aveugle et tu finis par aimer le mal en croyant que c'est un bien. Si tu sors le vase (ton âme) de la fontaine (Dieu) pour donner à boire aux autres, il se vide vite. Si tu es triste car on ne te rend pas ton amour, c'est que ton amour est encore "imparfait" et centré sur toi.`,
+  `Si ne-t sèrbes pas de la toûe rasoû ta cerca la Bertat, que marches à l'abùglẹ e que bas ayma lou mau en credén qu'éy û bê. Si tournes sourti lou bachèth de la houn ta da à bébẹ aus àutẹs, que-s boéyte bìstẹ. Si qu'ès trìste permou qui ne-t rendém pas lou toû amou, qu'éy qui lou toû amou n'éy pas encoère perfèyt e qu'éy en-clabat sus tu.`,
 
-  `Dans la vie spirituelle, celui qui n'avance pas recule. Il faut passer de la peur du châtiment à l'amour de la vertu. Personne ne peut "sortir" de la main de Dieu. Soit on y demeure par la miséricorde (en l'aimant), soit on y demeure sous sa justice (en subissant les conséquences de ses actes). Si la tentation te déplaît, c'est la preuve que Dieu est en toi.`,
+  `Héns la bite spirituau, lou qui n'abance pas recule. Que cau passa de la pòu dou castic à l'amou de la bertut. Arré ne pot sourti de la mâ de Diu. Que demouren aciu per la misericorde ou debath la soûe yustìci. Si la tentacioû ne-t plats pas, qu'éy la probe de que Diu qu'éy déns tu.`,
 
-  `Dieu retire le sentiment de sa présence pour nous empêcher de devenir des "enfants gâtés" qui ne cherchent que les bonbons spirituels (chrétiens mercenaires) pour qu'on comprenne que le péché est un néant et qu'on retourne à lui. Comme une mère qui arrête d'allaiter son enfant pour qu'il commence à manger de la nourriture solide (la "chair" du Christ crucifié). Ne pas s'aimer soi-même à travers Dieu.`,
+  `Diu retìre lou sentimén de la soûe presénce ta-ns empacha de debiénẹ "maynats gastats" qui ne cèrcan soùnquẹ lous douçous spirituaus ta que coumprenìm qui lou pecat qu'éy boéyt e que-ns ne toùrnim ta lou. Coum ûe may qu'arreste d'apoupa lou soû maynat ta qui couménce à minya mascadure. Ne pas ayma-s éth-medich à trabèrs Diu.`,
 
-  `Un chrétien qui pèche est plus coupable qu'un païen car + on reçoit, + on est redevable. Le démon ne peut nous faire tomber que si nous donnons notre consentement. Dieu permet la tentation pour qu'on prouve notre amour et grandisse en connaissance de nous. Il faut vider ses attachements mondains pour que Dieu entre.`,
+  
+  
+  
+  `Û crestia qui peque qu'éy méy colupàble qu'û paga permou que méy que recebèm, méy que debem réndẹ. Lou demoun que-ns pot ha càdẹ soùnquẹ si dam lou noùstẹ counsentimen. Diu permet la tentaciou ta que-n proùbim lou noùstẹ amou e grandìm en counechénce de nousàutẹs. Que cau boeyta lous soûs ligames dou moùndẹ ta que Diu entre.`,
 
-  `Pour ceux qui ne peuvent pas pleurer physiquement (Dieu le fait pour t'empêcher de devenir orgueilleux ou de te satisfaire de ta propre émotion), c'est le désir ardent du cœur qui compte autant que l'eau des yeux (l'œil traduit ce que le cœur ressent, un cœur plein d'amour-propre donne des larmes égoïstes). La perfection, c'est de pleurer sur les péchés du monde et la perte des âmes.`,
+  `Taus qui nou pòdẹn pas ploura (Diu qu'at hè ta-t empacha de debiénẹ orgulhous ou de-t acountenta de la toûe emouciou), qu'éy lou desi ardent dou co qui coùmpte autâ que l'aygue dous oélhs (l'oélh qu'arrebire ço qui lou co sent, û co plé d'amou-propi da larmes egoïstes). La perfecciou qu'éy de ploura sus lous pecats dou moùndẹ e la pèrtẹ de las àmnes.`,
 
-  `Ceux qui attaquent l'Église ne peuvent pas atteindre Dieu, mais ils perdent la "grâce", ce qui les mène à la damnation s'ils ne se repentent pas. Toucher à un prêtre (ou par mépris), c'est offenser Dieu directement. Le prêtre est un trésor dans un vase d'argile.`,
+  `Lous qui atàquen la Glèyse ne pòdẹn pas touca Diu, més que pèrden la gràci, ço qu'ous mie ta la pèrtẹ si ne-s repénten pas. Touca ad û prèste (ou per despudi), qu'éy aufensat Diu directamén. Lou prèste qu'éy û tresor héns û bachèth de tèrre.`,
 
-  `Dieu donne à chacun les secours dont il a besoin, c'est le bon et vrai médecin. S'il permet une épreuve c'est pour ta sanctification. La faim, la soif, la perte d'argent, les insultes, le froid permettent un but précis : le salut. La sodomie ou la masturbation sont des abominations, même les démons (par noblesse de nature angélique) en sont horrifiés.`
+  `Diu da à chacû lous secours doun a besougn, qu'éy lou boû e bertadè medecî. Si permet ûe esprabe qu'éy ta la toûe santificacioû. La hàmi, la sét, la pèrt de mounéde, las enhountes, lou rét que pèrmetẹn û but precis : lou salut. La sodomie ou la masturbaciou que soun hàstis, medich lous demouns (per noublesse de nature anyoulique) que-n soun espaurits.`
+
+
+
 ]
 
 export default function CatherinePage() {
@@ -237,16 +246,17 @@ alt="Drapeau du Béarn"
           qui parla aux puissants</>
             ) : (  
  <>
- Une femme simple
+ Ûe hémble sìmple
           <br />
-          qui parla aux puissants 
+          qui parlè aus pouchans 
 
 </>
             )}
         </h2>
 
         <p>  {langue === 'fr'  ?
-          'Sainte Catherine de Sienne, simple religieuse laïque dominicaine du XIVe siècle, fut le personnage politique majeur qui fit revenir le pape à Rome.' : 'Sainte Catherine de Sienne, simple religieuse laïque dominicaine du XIVe siècle, fut le personnage politique majeur qui fit revenir le pape à Rome.'}
+          'Sainte Catherine de Sienne, simple religieuse laïque dominicaine du XIVe siècle, fut le personnage politique majeur qui fit revenir le pape à Rome.' : 
+          'Sénte Catherine de Sienne, sìmple reliyiouse laïque dominicane dou sècle quatoùrzau, qu\'estou lou persounàdyẹ poulitique màyẹ qui hesou tourna biénẹ lou pape ta Roume.'}
         </p>
       </div>
     </div>
@@ -264,12 +274,13 @@ alt="Drapeau du Béarn"
 
 
             {langue === 'fr'  ?
-          '« Vous n’avez pas été bien ferme. Je prie pour que vous agissiez en homme viril. Ayez faim du salut de vos brebis. »' : '« Vous n’avez pas été bien ferme. Je prie pour que vous agissiez en homme viril. Ayez faim du salut de vos brebis. »'}
+          '« Vous n’avez pas été bien ferme. Je prie pour que vous agissiez en homme viril. Ayez faim du salut de vos brebis. »' :
+           '« Ne p\'èt pas estats proû rèdẹs. Que prèghi ta qui pouchquiats ha coum û òmi d\'òmi. Ayit hami dou salut de las bòstẹs aulhes. »'}
         </p>
 
         <span className={styles.quoteAuthor}>
           {langue === 'fr'  ?
-          '  — Sainte Catherine au pape' : '  — Sainte Catherine au pape' }
+          '  — Sainte Catherine au pape' : '  — Sénte Catherine au pape' }
         </span>
       </div>
     </div>
@@ -294,11 +305,16 @@ alt="Drapeau du Béarn"
           reconnaissant dans ses paroles
           une sagesse inspirée
           par l’Esprit Saint. </>   ) : (  
- <>      L’Église l’a proclamée
-          Docteur de l’Église,
-          reconnaissant dans ses paroles
-          une sagesse inspirée
-          par l’Esprit Saint.   </>  )}
+ <>  
+    Ûe hémble chéns enstruccioû unibersitàri, chéns n’abé pas jaméy estudiat héns ûe escole de theoulouyìe ni oubèrt de manuau de drét canoun, qu’estou toutû recounéchude coum tout à fèyt counfourme à l’ourtoudoussìe après l’ispeccioû dous soûs escriuts.
+          <br />
+          <br />
+
+    La Glèyse que la prouclamade
+          Douctur de la Glèyse,
+          recounéchén héns las soûes paraules
+          ûe sayesse enspirade
+          per l’Esperit Sén.   </>  )}
         </p>
       </div>
     </div>
@@ -308,7 +324,7 @@ alt="Drapeau du Béarn"
   <section className={styles.learningSection}>
     <div className="container">
       <h2 className={styles.sectionTitle}> {langue === 'fr'  ?
-        'Enseignements spirituels' :   'Enseignements spirituels' }
+        'Enseignements spirituels' :   'Ensegnaméns spirituèls' }
       </h2>
 
       <div className={styles.learningGrid}>
@@ -347,7 +363,7 @@ alt="Drapeau du Béarn"
          
       {langue === 'fr'  ?    
          
-          '« La Vierge Marie à Sainte Bernadette (souvent malade) : Je ne vous promets pas de vous rendre heureuse dans ce monde, mais dans l’autre. »' : '« La Vierge Marie à Sainte Bernadette (souvent malade) : Je ne vous promets pas de vous rendre heureuse dans ce monde, mais dans l’autre. »'
+          '« La Vierge Marie à Sainte Bernadette (souvent malade) : Je ne vous promets pas de vous rendre heureuse dans ce monde, mais dans l’autre. »' : '« La Bièrye Marie à Sénte Bernadette (soubén malaude) : Ne-p prouméti pas de-p réndẹ urouse héns aquéstẹ moùndẹ, més héns l\'àutẹ. »'
   }  </p>
       </div>
     </div>
