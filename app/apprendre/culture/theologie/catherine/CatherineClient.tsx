a87dc40b008f0cccb07fed4833ac09f576a0c899
medericc <bleublanctour1@>
2026-07-02 20:31:55 +0200

    ee

--- app/apprendre/culture/theologie/catherine/CatherineClient.tsx
+++ app/apprendre/culture/theologie/catherine/CatherineClient.tsx
@@ -8,15 +8,14 @@ import styles from './catherine.module.css'
 import Image from 'next/image'
 
 const teachingsF = [
-  `Ne pas corriger un pécheur par peur ou polir ses phrases par confort social, c'est comme un médecin qui met un pansement sur une plaie purulente sans la nettoyer. On peut même accomplir soi-même sa pénitence car ce péché est seulement endormi en nous.`,
+  `L'amour-propre est le poison du monde et produit tous les péchés. Ne pas corriger un pécheur par peur ou polir ses phrases par confort social, c'est comme un médecin qui met un pansement sur une plaie purulente sans la nettoyer. On peut même accomplir soi-même sa pénitence car ce péché est seulement endormi en nous.`,
 
-  `L'amour-propre est le poison du monde et produit tous les péchés. Il faut vouloir le salut des âmes et faire la volonté de Dieu, pas la sienne. Tout le bien et le mal se font par l'intermédiaire du prochain.`,
 
   `Les pécheurs en tentant les hommes deviennent les instruments qui éprouvent et renforcent la vertu des serviteurs de Dieu. La vertu dans la difficulté a + de valeur que dans la joie, comme un jardinier qui perd son jardin et continue de travailler la terre (il aime vraiment). Un laïc qui obéit avec un immense amour a plus de mérite qu'un religieux qui obéit par simple habitude.`,
 
   `On ne peut pas suivre le Christ à notre convenance il faut suivre son chemin d'humilité. Les pécheurs souffrent comme les saints mais leur souffrance est inutile. Un petit acte fait avec charité a + de poids que des souffrances subies sans amour. On souffre non pas pour soi, mais par amour pour Dieu et par douleur de voir les autres s'égarer.`,
 
-  `Ne jamais juger la volonté des autres et ne se sentir offensé que par celui qui offense Dieu. Si Dieu te fait sentir la souffrance de cette âme, c'est pour que tu l'aides. Même une pensée égoïste nuit au prochain, tu le prives d'une prière.`,
+  `Ne jamais juger la volonté des autres et ne se sentir offensé que par celui qui offense Dieu. Si Dieu te fait sentir la souffrance de cette âme, c'est pour que tu l'aides. Même une pensée égoïste nuit au prochain, tu le prives d'une prière. Il faut vouloir le salut des âmes et faire la volonté de Dieu, pas la sienne. `,
 
   `Dieu veut que le riche ait besoin du pauvre et que le savant ait besoin de l'ignorant. Pour nous forcer à la charité. Si tu avais tous les talents, tu n'aurais pas besoin des autres et tu tomberais dans l'orgueil. Dieu nous utilise comme ses mains (ministres) pour distribuer ses dons. Si tu as quelque chose, c'est pour le donner à celui qui ne l'a pas.`,
 
@@ -30,15 +29,14 @@ const teachingsF = [
 
   `Pour ceux qui ne peuvent pas pleurer physiquement (Dieu le fait pour t'empêcher de devenir orgueilleux ou de te satisfaire de ta propre émotion), c'est le désir ardent du cœur qui compte autant que l'eau des yeux (l'œil traduit ce que le cœur ressent, un cœur plein d'amour-propre donne des larmes égoïstes). La perfection, c'est de pleurer sur les péchés du monde et la perte des âmes.`,
 
-  `Ceux qui attaquent l'Église ne peuvent pas atteindre Dieu, mais ils perdent la "grâce", ce qui les mène à la damnation s'ils ne se repentent pas. Toucher à un prêtre (ou par mépris), c'est offenser Dieu directement. Le prêtre est un trésor dans un vase d'argile.`,
+  `Ceux qui attaquent l'Église ne peuvent pas atteindre Dieu, mais ils perdent la "grâce", ce qui les mène à la damnation s'ils ne se repentent pas. Toucher à un prêtre (ou par mépris), c'est offenser Dieu directement. Le prêtre est un trésor dans un vase d'argile. Tout le bien et le mal se font par l'intermédiaire du prochain.`,
 
   `Dieu donne à chacun les secours dont il a besoin, c'est le bon et vrai médecin. S'il permet une épreuve c'est pour ta sanctification. La faim, la soif, la perte d'argent, les insultes, le froid permettent un but précis : le salut. La sodomie ou la masturbation sont des abominations, même les démons (par noblesse de nature angélique) en sont horrifiés.`
 ]
 
 const teachingsB = [
-  `Ne pas courridya û pecadou per pòu ou dousseya las soûes frases per counfòrt souciau, qu'éy coum û medecî qui hique û pansamén sus ûe plague pudentisse chéns la neteya. Que poudém médich ha nous-medich la soûe penitence permou qu'aquéste pecat qu'éy soulamen adroumit déns nous.`,
+  `Ne pas courridya û pecadou per pòu ou dousseya las soûes frases per counfòrt souciau, qu'éy coum û medecî qui hique û pansamén sus ûe plague pudentisse chéns la neteya. Que poudém médich ha nous-medich la soûe penitence permou qu'aquéste pecat qu'éy soulamen adroumit déns nous. L'amou-pròpi qu'éy lou pousoû dou moùndẹ e que produsech touts lous pecats.`,
 
-  `L'amou-pròpi qu'éy lou pousoû dou moùndẹ e que produsech touts lous pecats. Que cau boulé lou salut de las àmnes e ha la boulentat de Diu, noû pas la soûe. Tout lou bê e lou mau se hèn per lou prouchén.`,
 
   `Lous pecadous en tentan lous òmis que debiénen lous utìs qui hìquen à l'espròbe e qu'agalharden la bertut dous serbitous de Diu. La bertut déns lou mauaysit qu'a méy de balou que déns la gauyou, coum û yardinè qui pèr lou soû casau e countinue de tribalha la tèrre. Û laïque qui aubedi dap û gran amou qu'a méy de merite qu'û reliyious qui aubedi per abitude.`,
 
@@ -47,7 +45,7 @@ const teachingsB = [
  
  
  
-  `Ne pas jaméy yudyat la boulentat dous àutẹs e ne-s senti aufensat soùnquẹ p'ou qui aufénse Diu. Si Diu que-t hè senti la doulou d'aquére àmne, qu'éy ta que-t l'àydis. Medich ûe pensade egoïste que hè dou mau au prouchén, qu'ou pribes d'ûe pregàri.`,
+  `Ne pas jaméy yudyat la boulentat dous àutẹs e ne-s senti aufensat soùnquẹ p'ou qui aufénse Diu. Si Diu que-t hè senti la doulou d'aquére àmne, qu'éy ta que-t l'àydis. Medich ûe pensade egoïste que hè dou mau au prouchén, qu'ou pribes d'ûe pregàri. Que cau boulé lou salut de las àmnes e ha la boulentat de Diu, noû pas la soûe.`,
 
   `Diu bòu que lou rìche àyi besougn dou pràubẹ e qu'ou saben àyi besougn de l'ignouren. Ta-ns hourça à la caritat. Si abès touts lous talénts, ne-t caleré pas lou moùndẹ e que caderés héns l'ourgul. Diu que-ns emplegue coum las soûes mâs ta desparti lous soûs dats. Si qu'as quàuqu'arré, qu'éy ta-u da au qui n'a pas.`,
 
@@ -64,7 +62,7 @@ const teachingsB = [
 
   `Taus qui ne pòdẹn pas ploura (Diu qu'at hè ta-t empecha de debiénẹ orgulhous ou de-t acountenta de la toûe emoucioû), qu'éy lou desi ardent dou co qui coùmpte autâ que l'aygue dous oélhs (l'oélh qu'arrebire ço qui lou co sen, û co plé d'amou-pròpi da larmes egoïstes). La perfeccioû qu'éy de ploura sus lous pecats dou moùndẹ e la pèrtẹ de las àmnes.`,
 
-  `Lous qui atàquen la Glèyse ne pòdẹn pas touca Diu, mé que pèrden la gràci, ço qu'ous mie ta la pèrtẹ si ne-s repénten pas. Touca a û prèste (ou per despudi), qu'éy aufensat Diu directamén. Lou prèste qu'éy û tresor héns û bachèth de tèrre.`,
+  `Lous qui atàquen la Glèyse ne pòdẹn pas touca Diu, mé que pèrden la gràci, ço qu'ous mie ta la pèrtẹ si ne-s repénten pas. Touca a û prèste (ou per despudi), qu'éy aufensat Diu directamén. Lou prèste qu'éy û tresor héns û bachèth de tèrre. Tout lou bê e lou mau se hèn per lou prouchén.`,
 
   `Diu da à chacû lous secours doun a besougn, qu'éy lou boû e beritàblẹ medecî. Si permé ûe espròbe qu'éy ta la toûe santificacioû. La hàmi, la sét, la pèrte de mounéde, las enhountes, lou rét que pèrmetẹn û but precis : lou salut. La sodomie ou la masturbacioû que soun hàstis, medich lous demouns (per noublesse de nature anyoulique) que-n soun espaurits.`
 
